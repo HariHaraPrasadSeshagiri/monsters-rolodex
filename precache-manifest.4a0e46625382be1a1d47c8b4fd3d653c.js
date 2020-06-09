2356b7cdb5af426e02b3db5d4ab8f6f2541d6b8c
Seshagiri H Prasad <Seshagiri.Prasad@in.ey.com>
2020-06-09 09:37:03 +0530

    Updates

---
+++ precache-manifest.4a0e46625382be1a1d47c8b4fd3d653c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c19818aa52d880d1a812e7846b719fb",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "03a45e70d5515933ea45",
+    "url": "/monster-rolodex/static/css/main.7c3adaed.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03a45e70d5515933ea45",
+    "url": "/monster-rolodex/static/js/main.bf5c840b.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
